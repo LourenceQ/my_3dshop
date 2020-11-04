422e3d18b6d45cd1ad8f31e82b45b432acee1651
Louernce <lqfmecatronica@gmail.com>
2020-11-04 05:38:22 -0300

    Populated models database and admin.py

--- shop_pro/shop_app/admin.py
+++ shop_pro/shop_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Categoria, Produto
 
 # Register your models here.
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ['nome','slug']
+    preopupulated_fields = {'slug':('name',)}
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['nome','slug','preco','disponivel','criado','atualizado']
+    list_filter = ['disponivel','criado','atualizado']
+    list_editable = ['preco','disponivel']
+    preopopulated_fields = {'slug':('nome',)}
--- shop_pro/shop_app/models.py
+++ shop_pro/shop_app/models.py
@@ -6,15 +6,15 @@ class Categoria(models.Model):
     slug = models.SlugField(max_length=200,unique=True)
 
     class Meta:
-        ordernar = ('nome',)
-        verbose_nome = 'categoria'
-        verbose_nome_plural = 'categorias'
+        ordering = ('nome',)
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
 
     def __str__(seflf):
         return self.name
 
 class Produto(models.Model):
-    categoria = models.ForeignKey(Categoria, realeted_name='produtos',on_delete=models.CASCADE)
+    categoria = models.ForeignKey(Categoria, related_name='produtos',on_delete=models.CASCADE)
     nome = models.CharField(max_length=200, db_index=True)
     slug = models.SlugField(max_length=200, db_index=True)
     imagem = models.ImageField(upload_to='products/%Y/%m/%d',blank=True)
@@ -22,11 +22,11 @@ class Produto(models.Model):
     preco = models.DecimalField(max_digits=10, decimal_places=2)
     disponivel = models.BooleanField(default=True)
     criado = models.DateTimeField(auto_now_add=True)
-    atualizado  models.DateTimefield(auto_now=True)
+    atualizado = models.DateTimeField(auto_now=True)
 
     class Meta:
-        ordernar = ('name',)
-        index_junto = (('id', 'slug'),)
+        ordering = ('nome',)
+        index_together = (('id', 'slug'),)
 
     def __str_(self):
         return self.name
